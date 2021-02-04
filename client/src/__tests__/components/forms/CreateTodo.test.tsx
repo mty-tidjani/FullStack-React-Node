31d8cbfb47df8c91c7ad6a56329e5ad7dab8c956
MOMO Tidjani <mty.tidjani@gmail.com>
2021-02-04 11:57:26 +0100

    Small changes

--- client/src/__tests__/components/forms/CreateTodo.test.tsx
+++ client/src/__tests__/components/forms/CreateTodo.test.tsx
@@ -7,16 +7,11 @@ import { Todo } from '../../../types/models';
 
 describe('CreateTodo', () => {
   let wrapper: ShallowWrapper<any>;
-  const todo: Todo = { _id: 'xxx', nm: 'MOMO' };
   beforeEach(() => {
     wrapper = shallow(<CreateTodo />);
   });
 
   it('CreateTodo renders without crash', () => {
     expect(wrapper.find('.create_todo')).toHaveLength(1);
-  });
-
-  it('Should check if CreateTodo renders todo Name', () => {
-    expect(wrapper.find('.create_todo').text()).toContain(todo.nm);
   });
 });
